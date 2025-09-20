9ef3cbff06bcd88ac79910f7225edaf3409db7fa
Hiroka Yamada <suiren.1217@gmail.com>
2025-09-20 15:07:24 +0900

    一旦保存

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,9 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/products', function () {
-    return view('products');
-});
+Route::get('/products', [ProductController::class, 'index'])->name('products.index');
 Route::get('/register', function () {
     return view('register');
 });
